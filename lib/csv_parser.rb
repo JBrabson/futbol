7f754cbdb04fb95a9ebdd7db30224e7715591de3
brueck1988 <brueck1988@gmail.com>
2021-02-26 21:40:50 -0700

    Added csv_parser

--- lib/csv_parser.rb
+++ lib/csv_parser.rb
@@ -3,51 +3,35 @@ require 'CSV'
 
 class CsvParser
   attr_reader :all_games,
-              :dummy_game_data
+              :all_teams,
+              :all_game_stats
 
-  def initialize(game_path)
+  def initialize
     @game_path = './data/games.csv'
-    @dummy_game_path = './test/games_dummy.csv'
     @team_path = './data/teams.csv'
     @game_stats_path = './data/game_stats.csv'
-    @dummy_game_data = []
     @all_games = all_games
+    @all_teams = all_teams
+    @all_game_stats = all_game_stats
   end
 
   def create_game_data
     @all_games = []
     CSV.foreach(@game_path, headers: true, header_converters: :symbol) do |row|
-      game_id = row[:game_id]
-      season = row[:season]
-      type = row[:type]
-      date_time = row[:date_time]
-      away_team_id = row[:away_team_id].to_i
-      home_team_id = row[:home_team_id].to_i
-      away_goals = row[:away_goals].to_i
-      home_goals = row[:home_goals].to_i
-      venue = row[:venue]
-      venue_link = row[:venue_link]
-      @all_games << Game.new(game_id, season, type, date_time, away_team_id,
-        home_team_id, away_goals, home_goals, venue, venue_link)
+      @all_games << Game.new(row)
     end
-    x = GameData.new(@all_games)
   end
 
-  def create_dummy_game_data
-    CSV.foreach(@dummy_game_path, headers: true, header_converters: :symbol) do |row|
-      game_id = row[:game_id]
-      season = row[:season]
-      type = row[:type]
-      date_time = row[:date_time]
-      away_team_id = row[:away_team_id].to_i
-      home_team_id = row[:home_team_id].to_i
-      away_goals = row[:away_goals].to_i
-      home_goals = row[:home_goals].to_i
-      venue = row[:venue]
-      venue_link = row[:venue_link]
-      @dummy_game_path << Game.new(game_id, season, type, date_time, away_team_id,
-        home_team_id, away_goals, home_goals, venue, venue_link)
+  def create_team_data
+    @all_games = []
+    CSV.foreach(@team_path, headers: true, header_converters: :symbol) do |row|
+      @all_teams << Team.new(row)
+  end
+
+  def create_game_stats_data
+    @all_games = []
+    CSV.foreach(@game_stats_path, headers: true, header_converters: :symbol) do |row|
+      @all_game_stats << GameStats.new(row)
     end
-    y = GameData.new(@dummy_game_path)
   end
 end
